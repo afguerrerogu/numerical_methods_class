538538c69379bf6000e2d86f25e3df0359fe2f48
andres <afguerrerogu@unal.edu.co>
2019-09-07 20:41:54 -0500

    este es un programa que interactua con el usuario

---
+++ 2019-09-04-intro/leer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void)
+{
+	double x=0.0;
+	std:: cout <<"escribe el numero :"<< std:: endl;
+ 	std::cin >> x;
+	std::cout <<"he leido : "<< x << std:: endl;	
+	return 0;
+}
+
