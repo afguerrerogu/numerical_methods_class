0e40f3b26f7fd8067f2fccb56314beb844c43a24
afguerrerogu <afguerrerogu@unal.edu.co>
2019-10-18 21:53:41 -0500

    ejercicio 50 del proyecto euler

--- ejercicios/projeu50.cpp
+++ ejercicios/projeu50.cpp
@@ -7,7 +7,55 @@ The longest sum of consecutive primes below one-thousand that adds to a prime, c
 
 Which prime, below one-million, can be written as the sum of the most consecutive primes?*/
 
-#include<cstudio>
+#include<cstdio>
+#include<cmath>
+#include <fstream>
+#include<iostream>
 
+int isprime(int m);
 
+int main(void){
+  
+  std::ofstream fout ("euler50.txt");
+	int n;
+	int sum;
+	int numero;
+	
+	for(int jj=2;jj<6000;++jj){
+	  sum=0;
+	  numero=0;
+//	  fout<<"este es el numero inicial"<<"\t"<< jj<<"\n";
+		for(int ii=jj;ii<6000;++ii){
+//			fout<<ii<<"\n";
+			if(isprime(ii)==1){
+//				fout<<"el num es prim"<< ii<<"\n";
+				sum=sum+ii;
+			  numero=numero+1;
+			  }
+//  			fout <<"suma"<<sum<<"\n";
+			if (isprime(sum)==1 and sum<1000000 and isprime(ii)==1 ){
+				fout<<sum<<"\t"<<"numeros sumados"<<"\t"<<numero<<std::endl;
+			}
+		}
+		
+//		fout<<sum<<"\t"<<"numero de primos sumados"<<"\t"<<numero<<"\n";
+  }
+	//fout<<n<<"\n";
+	
+	fout.close();
+	return 0;
+}
+
+int isprime(int m)
+{
+	  if (m == 1) return 0;
+	    int var = 1;
+	      for(int ii = 2; ii <= std::sqrt(m); ++ii) {
+		          if (m%ii==0) {
+				        var = 0;
+					break;
+					          }
+			    }
+	        return var;
+}
 
